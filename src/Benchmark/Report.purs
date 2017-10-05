82843a31c12b1e97455041ca85caecc628164fa1
Jorge Acereda <jacereda@gmail.com>
2017-10-05 17:37:49 +0200

    report URL

--- src/Benchmark/Report.purs
+++ src/Benchmark/Report.purs
@@ -12,7 +12,7 @@ import Simple.JSON (writeJSON)
 type ReportEff e = OpenURLEff e
 
 report1 :: forall e. BenchRes -> Eff (ReportEff e) Unit
-report1 br = openURL $ "http://localhost:8000?" <> (encodeURIComponent $ writeJSON br)
+report1 br = openURL $ "http://jacereda.github.io/report/?" <> (encodeURIComponent $ writeJSON br)
 
 report :: forall e. Array BenchRes -> Eff (ReportEff e) Unit
 report brs = sequence_ $ report1 <$> brs
